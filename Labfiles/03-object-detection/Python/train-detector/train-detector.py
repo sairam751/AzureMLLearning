0506385c96d4d99f4068ff5dd36539e55c3582b2
sairam751 <sairam751348@gmail.com>
2025-04-05 10:43:18 +0530

    objectdetection

--- Labfiles/03-object-detection/Python/train-detector/train-detector.py
+++ Labfiles/03-object-detection/Python/train-detector/train-detector.py
@@ -16,6 +16,9 @@ def main():
         training_endpoint = os.getenv('TrainingEndpoint')
         training_key = os.getenv('TrainingKey')
         project_id = os.getenv('ProjectID')
+        print("Training Endpoint: ", training_endpoint)
+        print("Training Key: ", training_key)
+        print("Project ID: ", project_id)
 
         # Authenticate a client for the training API
         credentials = ApiKeyCredentials(in_headers={"Training-key": training_key})
@@ -32,41 +35,48 @@ def main():
 
 
 def Upload_Images(folder):
-    print("Uploading images...")
+    try:
+        print("Uploading images...")
 
-    # Get the tags defined in the project
-    tags = training_client.get_tags(custom_vision_project.id)
+        # Get the tags defined in the project
+        tags = training_client.get_tags(custom_vision_project.id)
+        print("Tags: ", tags)
 
-    # Create a list of images with tagged regions
-    tagged_images_with_regions = []
+        # Create a list of images with tagged regions
+        tagged_images_with_regions = []
 
-    # Get the images and tagged regions from the JSON file
-    with open('tagged-images.json', 'r') as json_file:
-        tagged_images = json.load(json_file)
-        for image in tagged_images['files']:
-            # Get the filename
-            file = image['filename']
-            # Get the tagged regions
-            regions = []
-            for tag in image['tags']:
-                tag_name = tag['tag']
-                # Look up the tag ID for this tag name
-                tag_id = next(t for t in tags if t.name == tag_name).id
-                # Add a region for this tag using the coordinates and dimensions in the JSON
-                regions.append(Region(tag_id=tag_id, left=tag['left'],top=tag['top'],width=tag['width'],height=tag['height']))
-            # Add the image and its regions to the list
-            with open(os.path.join(folder,file), mode="rb") as image_data:
-                tagged_images_with_regions.append(ImageFileCreateEntry(name=file, contents=image_data.read(), regions=regions))
+        # Get the images and tagged regions from the JSON file
+        with open('tagged-images.json', 'r') as json_file:
+            tagged_images = json.load(json_file)
+            for image in tagged_images['files']:
+                # Get the filename
+                file = image['filename']
+                # Get the tagged regions
+                regions = []
+                for tag in image['tags']:
+                    print("Tag: ", tag)
+                    tag_name = tag['tag']
+                    # Look up the tag ID for this tag name
+                    print("Tag Name: ", tag_name)
+                    tag_id = next(t for t in tags if t.name == tag_name).id
+                    print("Tag ID: ", tag_id)
+                    # Add a region for this tag using the coordinates and dimensions in the JSON
+                    regions.append(Region(tag_id=tag_id, left=tag['left'],top=tag['top'],width=tag['width'],height=tag['height']))
+                    print("Region: ", regions)
+                # Add the image and its regions to the list
+                with open(os.path.join(folder,file), mode="rb") as image_data:
+                    tagged_images_with_regions.append(ImageFileCreateEntry(name=file, contents=image_data.read(), regions=regions))
 
-    # Upload the list of images as a batch
-    upload_result = training_client.create_images_from_files(custom_vision_project.id, ImageFileCreateBatch(images=tagged_images_with_regions))
-    # Check for failure
-    if not upload_result.is_batch_successful:
-        print("Image batch upload failed.")
-        for image in upload_result.images:
-            print("Image status: ", image.status)
-    else:
-        print("Images uploaded.")
-
+        # Upload the list of images as a batch
+        upload_result = training_client.create_images_from_files(custom_vision_project.id, ImageFileCreateBatch(images=tagged_images_with_regions))
+        # Check for failure
+        if not upload_result.is_batch_successful:
+            print("Image batch upload failed.")
+            for image in upload_result.images:
+                print("Image status: ", image.status)
+        else:
+            print("Images uploaded.")
+    except Exception as ex:
+        print(ex)
 if __name__ == "__main__":
     main()
