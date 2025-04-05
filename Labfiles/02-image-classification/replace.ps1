94372882a059af108f2055b9cd4a934ebb653576
sairam751 <sairam751348@gmail.com>
2025-04-05 09:39:53 +0530

    Imageclassfication coco file generation

--- Labfiles/02-image-classification/replace.ps1
+++ Labfiles/02-image-classification/replace.ps1
@@ -1,2 +1,3 @@
+#// absolute_url will be changed to the blob storage URL after runing this power shell script and coco file is modified in trainingimages folder
 $storageAcct = 'customclassifyssrr'
 (Get-Content training-images/training_labels.json) -replace '<storageAccount>', $storageAcct | Out-File training-images/training_labels.json
